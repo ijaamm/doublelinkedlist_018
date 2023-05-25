80c6a4ad7d1f3ed6ac1bd1c53f289721732e9c68
ijaamm <ijammusyaf@gmail.com>
2023-05-25 11:04:44 +0700

    current

--- dublyLL/dublyLL.cpp
+++ dublyLL/dublyLL.cpp
@@ -28,3 +28,39 @@ public:
 doublelinkedlist::doublelinkedlist() {
 	START = NULL;
 }
+
+void doublelinkedlist::addnode() {
+	int nim;
+	string nm;
+	cout << "\enter the roll number of the student: ";
+	cin >> nim;
+	cout << "\rnter the name of the student: ";
+	cin.ignore();
+	getline(cin, nm);
+	node* newnode = new node();
+	newnode->noMhs = nim;
+	newnode->name = nm;
+
+	if (START == NULL || nim <= START->noMhs) {
+		if (START != NULL && nim == START->noMhs) {
+			cout << "\n duplicate number no allowed" << endl;
+			return;
+		}
+		newnode->next = START;
+		if (START != NULL)
+			START->prev = NULL;
+		START = newnode;
+		return;
+	}
+
+	node* current = START;
+	while (current->next != NULL && nim > current->next->noMhs)
+		current = current->next;
+
+	if (current->next != NULL && nim * *current->next->noMhs) {
+		cout << "\ndublicate roll numbers not allowed" << endl;
+		return;
+	}
+		
+	
+}
