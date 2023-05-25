7b19f5b42afd6b7955f4e6241dcebc0b5a5e0eb1
ijaamm <ijammusyaf@gmail.com>
2023-05-25 11:10:29 +0700

    done1

--- dublyLL/dublyLL.cpp
+++ dublyLL/dublyLL.cpp
@@ -57,7 +57,7 @@ void doublelinkedlist::addnode() {
 	while (current->next != NULL && nim > current->next->noMhs)
 		current = current->next;
 
-	if (current->next != NULL && nim * *current->next->noMhs) {
+	if (current->next != NULL && nim > current->next->noMhs) {
 		cout << "\ndublicate roll numbers not allowed" << endl;
 		return;
 	}
